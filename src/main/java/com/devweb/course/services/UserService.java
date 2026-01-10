a67c6c37e9947f0a0721ded30ea868861c4392a7
mar1a-ed <medu.correa29@gmail.com>
2026-01-10 11:38:07 -0300

    Update

---
+++ src/main/java/com/devweb/course/services/UserService.java
@@ -0,0 +1,26 @@
+package com.devweb.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.devweb.course.entities.User;
+import com.devweb.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+}
